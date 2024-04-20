bd9a34a1bbe0434ae21a974db82e474913993678
Laura Long <lauralonggone@gmail.com>
2024-04-19 22:42:40 -0600

    Render 'no comments found' when search renders no responses

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -30,7 +30,7 @@ function Search({ setSearchTerm, setFilter }) {
             onChange={handleFilter}
             aria-label="Filter"
           >
-            <option value="all">All responses</option>
+            <option value="all">all comments</option>
             <option value="gc-residents">Moab residents</option>
           </select>
         </div>

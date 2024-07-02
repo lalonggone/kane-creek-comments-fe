464abeaccc131840c6dc5148d4e4e18fdcbf1780
Laura Long <lauralonggone@gmail.com>
2024-07-02 11:56:45 -0600

    Add About page with responsive design and embedded links for front-end and back-end repositories. Update Footer to include navigation links. Improve text styling and interactivity”

---
+++ src/components/About/About.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import './About.scss';
+
+const About = () => (
+  <>
+    <div className="about-container">
+      <h1>about this app</h1>
+      <p>
+        Kane Creek Comments is a platform that allows users to
+        filter, search through and display the thousands of petition survey
+        responses to the contentious Kane Creek development in Grand County,
+        Utah. The goal is to promote transparency and foster community
+        engagement by making these opinions accessible. Hopefully
+        key stakeholders like investors and elected officials will take these
+       into account when making decisions.
+      </p>
+      <p>
+        Notice how the majority of residents rate their level of concern at 5/5.
+        We recommend searching terms like “grew up in Moab”, “local business”,
+        or “river guide” to understand the variety of community
+        feedback.
+      </p>
+      <p>
+        This is a hand-rolled, full-stack web app built using React, TypeScript, SCSS,
+       Node.js, Express, PostgreSQL and lots of love. The{' '}
+        <a className="link"
+          href="https://github.com/lalonggone/kane-creek-comments-fe"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          front end
+        </a>{' '}
+        is deployed on Vercel and the{' '}
+        <a className="link"
+          href="https://github.com/lalonggone/kane-creek-comments-api"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          back end
+        </a>{' '}
+        is hosted on Render. Feel free to reach out if you have any questions / feedback or to collaborate!
+      </p>
+      <p>
+        Visit {' '}
+        <a className="link"
+          href="https://www.kanecreekwatch.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          kanecreekwatch.org
+        </a> {' '}
+        to learn more about the development and discover ways to get involved.
+      </p>
+    <Footer />
+    </div>
+  </>
+);
+
+export default About;
